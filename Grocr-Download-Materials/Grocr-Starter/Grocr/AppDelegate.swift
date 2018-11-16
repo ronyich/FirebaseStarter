bbf767c65cd7165c8a20e71fd654b4f14317923f
ronyich <ronyich0401@gmail.com>
2018-11-16 12:29:29 +0800

    First commit.

---
+++ Grocr-Download-Materials/Grocr-Starter/Grocr/AppDelegate.swift
@@ -0,0 +1,18 @@
+/// Copyright (c) 2018 Razeware LLC
+
+
+
+
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
---
+++ Grocr-Download-Materials/Grocr-Starter/Grocr/Controllers/LoginViewController.swift
@@ -0,0 +1,67 @@
+/// Copyright (c) 2018 Razeware LLC
+
+
+
+
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+  
+  // MARK: Constants
+  let loginToList = "LoginToList"
+  
+  // MARK: Outlets
+  @IBOutlet weak var textFieldLoginEmail: UITextField!
+  @IBOutlet weak var textFieldLoginPassword: UITextField!
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+  // MARK: Actions
+  @IBAction func loginDidTouch(_ sender: AnyObject) {
+    performSegue(withIdentifier: loginToList, sender: nil)
+  }
+  
+  @IBAction func signUpDidTouch(_ sender: AnyObject) {
+    let alert = UIAlertController(title: "Register",
+                                  message: "Register",
+                                  preferredStyle: .alert)
+    
+    let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+    }
+    
+    let cancelAction = UIAlertAction(title: "Cancel",
+                                     style: .cancel)
+    
+    alert.addTextField { textEmail in
+      textEmail.placeholder = "Enter your email"
+    }
+    
+    alert.addTextField { textPassword in
+      textPassword.isSecureTextEntry = true
+      textPassword.placeholder = "Enter your password"
+    }
+    
+    alert.addAction(saveAction)
+    alert.addAction(cancelAction)
+    
+    present(alert, animated: true, completion: nil)
+  }
+  
+}
+
+extension LoginViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if textField == textFieldLoginEmail {
+      textFieldLoginPassword.becomeFirstResponder()
+    }
+    if textField == textFieldLoginPassword {
+      textField.resignFirstResponder()
+    }
+    return true
+  }
+}
